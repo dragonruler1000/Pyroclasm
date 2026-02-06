ff6ff7606c2470d10282e0e96841577e551a0e18
dragonruler1000 <git@me.minecraftchest2.us>
2026-02-06 17:47:06 -0600

    fixed issue with last commit

--- src/main/java/us/minecraftchest2/pyroclasm/Pyroclasm.java
+++ src/main/java/us/minecraftchest2/pyroclasm/Pyroclasm.java
@@ -14,9 +14,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Pyroclasm.MOD_ID)
+@Mod(Pyroclasm.MODID)
 public class Pyroclasm {
-    public static final String MOD_ID = "pyroclasm";
+    public static final String MODID = "pyroclasm";
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public Pyroclasm() {
@@ -44,7 +44,7 @@ public class Pyroclasm {
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
